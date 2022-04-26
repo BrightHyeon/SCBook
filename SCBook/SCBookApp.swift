d1f4cd4d240dc0d705bdf8fe217d4cf4f59d78c8
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-04-26 23:40:56 +0900

    first commit

---
+++ SCBook/SCBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCBookApp.swift
+//  SCBook
+//
+//  Created by Hyeonsoo Kim on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SCBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
