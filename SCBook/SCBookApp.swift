20b2d8c5361bc446af4f364f7f90b115779d0381
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-04-30 01:57:09 +0900

    comment: Add comment

--- SCBook/SCBookApp.swift
+++ SCBook/SCBookApp.swift
@@ -26,7 +26,7 @@ struct SCBookApp: App {
  <TODO>
  // GCD DispatchTime 이용해서 초반 온보딩 로직 이상한거 수정하기. - cannot. but use animation - solve...
  
- 1. 스크롤 내린 후 Link타고 이동하면, LargeTitle이 잘려있음.!!... -> 그냥 이전 navigationStack과 이어지는듯?
+ 1. 스크롤 내린 후 Link타고 이동하면, LargeTitle이 잘려있음.!!... -> 그냥 이전 navigationStack과 이어지는듯? -> hmm....
  
  2. namespace HIGView를 스크롤내리는 행위로도 닫을 수 있도록하고, xmark버튼과 함께 움직이도록 해보기. like AppStoreApp - solve...
  3. Enum + @ViewBuilder사용해서, hig vs nohig 나누기.
@@ -39,5 +39,5 @@ struct SCBookApp: App {
  
  7. sidebar도 써보고프다.
  
- 8. Dark Mode용 Setting.
+ 8. Dark Mode용 Setting. -> solve.
  */
